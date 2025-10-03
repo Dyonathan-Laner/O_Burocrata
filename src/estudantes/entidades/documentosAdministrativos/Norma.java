3a3e2d69e844a3f2e38b6bc808d81f6de428cfaf
Gustavo Borges Arrussul Veiga <7663812@unipampa.edu.br>
2025-10-02 23:47:01 -0300

    PrimeiroCommitGus

--- src/estudantes/entidades/documentosAdministrativos/Norma.java
+++ src/estudantes/entidades/documentosAdministrativos/Norma.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package estudantes.entidades.documentosAdministrativos;
 
-/**
- *
- * @author Dyonathan
- */
 public class Norma {
+    private int numero;
+    private boolean valido;
+    private String texto;
     
 }
