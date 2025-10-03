142ab132161c7e622d45a68919521e6a841b2764
Dyonathan-Laner <dyonathanbl@hotmail.com>
2025-10-03 19:27:11 -0300

    primeiro_equals_hashcode

--- src/estudantes/entidades/Documento.java
+++ src/estudantes/entidades/Documento.java
@@ -7,15 +7,17 @@ import professor.entidades.CodigoCurso;
  * Classe que representa um documento genérico.
  * <br><br>
  * <strong>Seu trabalho começa aqui...</strong>
- * 
+ *
  * @author coloque os nomes dos autores aqui
  */
 public abstract class Documento {
+
     private String criador;
     private CodigoCurso codigoCurso;
     private int paginas;
-    
-    public Documento(String criador, CodigoCurso codigoCurso, int  paginas){
+
+    public Documento(String criador, CodigoCurso codigoCurso, int paginas) {
+        super();
         this.criador = criador;
         this.codigoCurso = codigoCurso;
         this.paginas = paginas;
@@ -32,17 +34,31 @@ public abstract class Documento {
     public int getPaginas() {
         return paginas;
     }
+
     @Override
-    public boolean equals(Object obj){
-        if(obj == null || getClass() != obj.getClass()){return false;}
-        
-        if(this == obj){return true;}
-        
-        Documento doc = (Documento)obj;
-        
-        return Objects.equals(this.criador, doc.criador) 
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        if (this == obj) {
+            return true;
+        }
+
+        Documento doc = (Documento) obj;
+
+        return Objects.equals(this.criador, doc.criador)
                 && Objects.equals(this.codigoCurso, doc.codigoCurso)
                 && this.paginas == doc.paginas;
     }
     
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 7 * hash + Objects.hashCode(this.criador);
+        hash = 7 * hash + Objects.hashCode(this.codigoCurso);
+        hash = 7 * hash + this.paginas;
+        return hash;
+    }
+
 }
